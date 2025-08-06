af9ccf36fd18d0a509b15409e2b4a2470ac3b161
Wagner Junior <wagner.junior30@gmail.com>
2025-08-06 01:18:42 -0300

    fix: tsconfig

---
+++ vetur.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  settings: {
+    "vetur.useWorkspaceDependencies": true,
+    "vetur.experimental.templateInterpolationService": true,
+  },
+  projects: [
+    {
+      root: "./src",
+      package: "./package.json",
+      tsconfig: "./tsconfig.json",
+      globalComponents: ["./src/components/**/*.vue"],
+    },
+  ],
+};
